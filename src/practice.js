97dfd5ee2b7d6e306742512285995beee3fddbe4
Sourik719 <97757109+Sourik719@users.noreply.github.com>
2022-11-10 18:30:58 +0530

    Update practice.js

--- src/practice.js
+++ src/practice.js
@@ -1,17 +1,17 @@
 export default [
     {
         name:"Bikram Basak",
-        image:"./Images/BIKU.jpeg",
+        image:"Images/BIKU.jpeg",
         review:"Soothing ambience, perfect food, different varieties, RRR is just awesome"
     },
     {
         name:"Arkaprabha Biswas",
-        image:"./Images/ARKA.jpeg",
+        image:"Images/ARKA.jpeg",
         review:"Had a great lunch with the combination of authentic Bengali and Mughlai preparations that too in a pocket friendly budget"
     },
     {
         name:"Niladri Banerjee",
-        image:"../Images/Niladri.jpeg",
+        image:"Images/Niladri.jpeg",
         review:"RRR should be your number one destination if you want to experience an authentic meal with great ambience"
     }
 ]
