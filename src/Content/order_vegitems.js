0ba7a4008b4966a7395333361b483a73db9d6661
Sourik719 <97757109+Sourik719@users.noreply.github.com>
2022-11-10 18:27:42 +0530

    Update order_vegitems.js

--- src/Content/order_vegitems.js
+++ src/Content/order_vegitems.js
@@ -3,13 +3,13 @@ export default [
                 name: "Paneer Butter Masala",
                 price: 220,
                 quantity: 10,
-                img: "../Images/PIC1.jpg"
+                img: "Images/PIC1.jpg"
             },
             {
                 name: "Butter Naan",
                 price: 20,
                 quantity: 50,
-                img: "../Images/butter_naan.jfif" 
+                img: "Images/butter_naan.jfif" 
             }
 ]
 
